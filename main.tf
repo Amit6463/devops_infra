b952130d2eb510109f5321e0f15450213f09ce48
Amit Kumar <amit@example.com>
2026-09-20 08:52:39 +0530

    resource8_add

--- main.tf
+++ main.tf
@@ -17,6 +17,11 @@ resource "azurerm_resource_group" "example" {
   location = "eastus"
 }
 
+resource "azurerm_resource_group" "example" {
+  name = "amit8_rg"
+  location = "westus"
+}
+
 resource "azurerm_storage_account" "stg" {
   name = "insiderstoarge45678"
   resource_group_name = azurerm_resource_group.example.name
